026e184ff8f53f5daae9e4cd1cc62cb4bd0cd635
ishani <91791452+deathmukh@users.noreply.github.com>
2023-04-29 13:39:50 +0530

    files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const handleModalClick = () => {
+    setModalVisible(true);
+  }
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+        <div class="logo-container">
+          <img src={process.env.PUBLIC_URL + '/assets/logo.jpg'} alt="logo" class="logo"/>
+          <h3>hello</h3>
+        </div>
+        
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+      <header>
+        <div className="header-content">
+          <h1>Welcome to Our Website</h1>
+          <h2>Learn more about our company and services</h2>
+          <button onClick={handleModalClick}>Click me!</button>
+          {modalVisible && (
+            <div className="modal">
+              <div className="modal-content">
+                <p>Thank you for your interest in our company!</p>
+                <button onClick={handleModalClose}>Close</button>
+              </div>
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
